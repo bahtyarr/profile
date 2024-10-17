44c5d0cff514880c62b298c77d440b970572967b
Bahtyar <bahtyar11@gmail.com>
2024-10-17 10:58:58 +0800

    fix path resource

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 const isProd = process.env.NODE_ENV === 'production';
+const isDevelopment = process.env.NODE_ENV === 'development';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd ? '/profile' : '',
+    // basePath: isDevelopment ? '' : '/profile', // uncomment this if local mode
+    basePath: '/profile',
     output: 'export',
     assetPrefix: isProd ? '/profile/' : '',
     images: {
