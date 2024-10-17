a8e41df6addf6ae58a539f1e9b6b952f48378780
Bahtyar <bahtyar11@gmail.com>
2024-10-17 09:42:07 +0800

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+    output: 'export',
+    assetPrefix: isProd ? '/profile/' : '',
+  };
 
-export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     distDir: 'dist',
+//     images: {
+//         unoptimized: true,
+//     },
+// };
+
+// module.exports = nextConfig;
