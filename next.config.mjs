b736d088bb1ba2a4a05f4054c2ee1a84abf3e280
Bahtyar <bahtyar11@gmail.com>
2024-10-17 10:51:21 +0800

    fix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd? '/profile' : '',
+    basePath: isProd ? '/profile' : '',
     output: 'export',
     assetPrefix: isProd ? '/profile/' : '',
     images: {
